812b78f3fccd3283c545d1464aa332af0aba4a7b
fomina <fk78745@gmail.com>
2026-05-17 15:10:21 +0300

    исправила урл

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './App.css'
 function App() {
   return (
     <LangProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/projectfominatulub">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
